e7a82074412f60c9567f15de5c01fa3ff2872a47
BuildTools <mariothrowsfieball@gmail.com>
2023-12-24 12:41:07 +0100

    load pdf

--- project/package/app.py
+++ project/package/app.py
@@ -1,5 +1,9 @@
 import sys
 from PyQt6 import QtWidgets, uic
+from PyQt6.QtCore import QUrl
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtWebEngineCore import QWebEnginePage
+from PyQt6.QtWidgets import QVBoxLayout
 from package.UI.MainWindow import Ui_MainWindow
 
 
@@ -9,12 +13,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.addDocument.clicked.connect(self.add_document)
         self.documents.itemClicked.connect(self.open)
+        self.webView = QWebEngineView()
+        self.webView.settings().setAttribute(self.webView.settings().WebAttribute.PluginsEnabled, True)
+        self.webView.settings().setAttribute(self.webView.settings().WebAttribute.PdfViewerEnabled, True)
 
     def add_document(self):
         fileName = QtWidgets.QFileDialog.getOpenFileName(self, 'Add Document', '', '(*.pdf)')
         list_item = QtWidgets.QListWidgetItem()
         list_item.setText(fileName[0])
         self.documents.addItem(list_item)
+        self.webView.setUrl(QUrl("file:///" + "/home/jovan/Documents/books/novels/german/Alexandre_Dumas-Der_Graf_von_Monte_Christo.pdf"))
+        self.setCentralWidget(self.webView)
         print(fileName)
 
     def open(self):
